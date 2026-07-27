318e6802ae0cf07918ba4d8d795a51c872de4f78
Abomination-ux <sasha.rudakov.1994@list.ru>
2026-07-27 05:55:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 # Токен теперь берется из переменной окружения, а не прописан в коде
-TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+if not TOKEN:
+    raise ValueError("Токен не задан! Установите переменную TELEGRAM_BOT_TOKEN")
 START_DATE = datetime(2026, 7, 27)
 CHANNEL_ID = -1001234567890   # ЗАМЕНИТЕ на реальный ID канала
 
